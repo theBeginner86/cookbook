ade454ac3a76b971c19b4ca4dce421d3f1d2694c
Pranav Singh <pranav.singh@intel.com>
2025-04-25 09:25:18 +0530

    update vllm script
    Signed-off-by: Pranav Singh <pranav.singh@intel.com>

--- infrastructure/gaudi/docker/vllm.sh
+++ infrastructure/gaudi/docker/vllm.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-docker run -d --runtime=habana -p 31090:22 
--e HABANA_VISIBLE_DEVICES=all
--e OMPI_MCA_btl_vader_single_copy_mechanism=none 
--v /mnt/hf_cache:/root/.cache/huggingface/hub 
---cap-add=sys_nice 
---ipc=host 
---name vllm-ALL 
--e http_proxy=$http_proxy -e HTTP_PROXY=$HTTP_PROXY -e https_proxy=$https_proxy
--e HTTPS_PROXY=$HTTPS_PROXY -e no_proxy=$no_proxy -e NO_PROXY=$NO_PROXY
--e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN
-vault.habana.ai/gaudi-docker/1.20.1/ubuntu24.04/habanalabs/pytorch-installer-2.6.0:latest 
+docker run -d --runtime=habana -p 31090:22 \
+-e HABANA_VISIBLE_DEVICES=all \
+-e OMPI_MCA_btl_vader_single_copy_mechanism=none \
+-v /mnt/hf_cache:/root/.cache/huggingface/hub  \
+--cap-add=sys_nice \
+--ipc=host \
+--name vllm-ALL \
+-e http_proxy=$http_proxy -e HTTP_PROXY=$HTTP_PROXY -e https_proxy=$https_proxy \
+-e HTTPS_PROXY=$HTTPS_PROXY -e no_proxy=$no_proxy -e NO_PROXY=$NO_PROXY \
+-e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
+vault.habana.ai/gaudi-docker/1.20.1/ubuntu24.04/habanalabs/pytorch-installer-2.6.0:latest \
 sh -c "
 apt-get update; 
 apt-get install;
